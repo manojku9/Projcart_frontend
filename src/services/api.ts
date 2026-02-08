fe0a29df598786435828db259bbeb30477f307a0
manojku9 <lopintimanoj957@gmail.com>
2026-02-09 00:20:15 +0530

    Updated changes

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const apiBaseUrl = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: apiBaseUrl
 });
 
 API.interceptors.request.use((req) => {
